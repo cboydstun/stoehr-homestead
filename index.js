9b4d2b8d2c9f69dfa157017f71394e4891bfe036
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2021-11-29 21:39:28 -0600

    added blog model/routes

--- index.js
+++ index.js
@@ -16,39 +16,48 @@ app.use(morgan(':method :url :response-time'))
 dotenv.config()
 
 //initalize a port
-const SERVER_PORT = process.env.PORT || 5002;
+const SERVER_PORT = process.env.PORT || 5002
 
 //connect to MongoDB
-mongoose.connect(process.env.MONGO_URL, {
+mongoose
+  .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
-    useUnifiedTopology: true
-}).then(console.log("Connected to MongoDB")).catch((err)=>{console.log(err)})
+    useUnifiedTopology: true,
+  })
+  .then(console.log('Connected to MongoDB'))
+  .catch((err) => {
+    console.log(err)
+  })
 
 //import routes
 import userRouter from './routes/users.js'
 import authRouter from './routes/auth.js'
 import todoRouter from './routes/todos.js'
+import blogRouter from './routes/blogs.js'
 
 //initalize routes
 app.use('/api/users', userRouter)
 app.use('/api/auth', authRouter)
 app.use('/api/todos', todoRouter)
+app.use('/api/blogs', blogRouter)
 
 //Heroku Deployment
-if(process.env.NODE_ENV === "production"){
-    app.use(express.static("client/build"))
-    app.get("*", (req, res)=>{
-        const __dirname = path.dirname(new URL(import.meta.URL).pathname)
-        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-    })
-}else{
-    app.get('/', (req, res)=>{
-        //basic greeting from operational API
-        res.send("The API is running...")
-    })
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'))
+  app.get('*', (req, res) => {
+    const __dirname = path.dirname(new URL(import.meta.URL).pathname)
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+} else {
+  app.get('/', (req, res) => {
+    //basic greeting from operational API
+    res.send('The API is running...')
+  })
 }
-    
+
 //app listening
-app.listen(SERVER_PORT, ()=>{console.log(`Server running at ${SERVER_PORT}`)})
+app.listen(SERVER_PORT, () => {
+  console.log(`Server running at ${SERVER_PORT}`)
+})
---
+++ routes/blogs.js
@@ -0,0 +1,68 @@
+//import dependencies
+const express = require('express')
+const router = express.Router()
+
+//import blog model
+import Blog from '../models/blog'
+
+//import auth middleware
+import auth from '../middleware/auth'
+
+//@GET - /api/blogs - get all blogs
+router.get('/', async (req, res) => {
+  try {
+    const blogs = await Blog.find()
+    res.json(blogs)
+  } catch (err) {
+    res.json({ message: err })
+  }
+})
+
+//@GET - /api/blogs/:id - get a single blog
+router.get('/:id', async (req, res) => {
+  try {
+    const blog = await Blog.findById(req.params.id)
+    res.json(blog)
+  } catch (err) {
+    res.json({ message: err })
+  }
+})
+
+//@POST - /api/blogs - create a new blog
+router.post('/', auth, async (req, res) => {
+  const blog = new Blog({
+    title: req.body.title,
+    content: req.body.content,
+  })
+  try {
+    const savedBlog = await blog.save()
+    res.json(savedBlog)
+  } catch (err) {
+    res.json({ message: err })
+  }
+})
+
+//@DELETE - /api/blogs/:id - delete a blog - only admin can delete
+router.delete('/:id', auth, async (req, res) => {
+  try {
+    const removedBlog = await Blog.remove({ _id: req.params.id })
+    res.json(removedBlog)
+  } catch (err) {
+    res.json({ message: err })
+  }
+})
+
+//@PUT - /api/blogs/:id - update a blog - only admin can update
+router.put('/:id', auth, async (req, res) => {
+  try {
+    const updatedBlog = await Blog.updateOne(
+      { _id: req.params.id },
+      { $set: { title: req.body.title, content: req.body.content } }
+    )
+    res.json(updatedBlog)
+  } catch (err) {
+    res.json({ message: err })
+  }
+})
+
+export default router
